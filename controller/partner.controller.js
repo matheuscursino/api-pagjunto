ea28839247bd2d6b5930654786def0e308c05fc4
matheuscursino <matheuscursino115@gmail.com>
2025-05-25 16:01:02 -0300

    added admin auth in post /partner

--- controller/partner.controller.js
+++ controller/partner.controller.js
@@ -1,11 +1,18 @@
 import partnerModel from '../model/partner.model.js'
+import adminModel from '../model/admin.model.js'
 import mongoose from 'mongoose'
 
 var partnerDoc;
+var adminDoc;
 var reqPartnerId;
+var reqAdminPassword;
 
 async function getPartnerDoc(){
     partnerDoc = await partnerModel.findOne({partnerId: reqPartnerId}).lean()
+}
+
+async function getAdminDoc(){
+    adminDoc = await adminModel.findOne({adminPassword: reqAdminPassword}).lean()
 }
 
 export function getPartner(req, res) {
@@ -16,7 +23,7 @@ export function getPartner(req, res) {
         if(partnerDoc == null){
             res.status(404).send()
         } else {
-            res.status(200).send(partnerDoc)
+            res.status(200).send()
         }
     })
 }
@@ -25,18 +32,24 @@ export function createPartner(req, res){
     var reqBodyValues = Object.values(req.body);
     var reqPartnerName = reqBodyValues[0]
     var reqPartnerPassword = reqBodyValues[1]
+    reqAdminPassword = reqBodyValues[2]
 
-    var partnerId = new mongoose.Types.ObjectId()
-    const partner = new partnerModel({
-        partnerId: partnerId,
-        password: reqPartnerPassword,
-        name: reqPartnerName,
-        accountBalance: 0
+    getAdminDoc().then(() => {
+        if(adminDoc == null){
+            res.status(403).send()
+        } else {
+            var partnerId = new mongoose.Types.ObjectId()
+            const partner = new partnerModel({
+                partnerId: partnerId,
+                password: reqPartnerPassword,
+                name: reqPartnerName,
+                accountBalance: 0
+            })
+            partner.save().then(() => {
+                res.status(201).send(partnerId)
+            })
+        }
     })
-    partner.save().then(() => {
-        res.status(201).send(partnerId)
-    })
-
 }
 
 export function updatePartnerBalance(req, res) {
