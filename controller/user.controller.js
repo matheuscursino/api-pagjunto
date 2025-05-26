26e335546c726af8f135a6f6f790e9975d94de1a
matheuscursino <matheuscursino115@gmail.com>
2025-05-26 00:02:08 -0300

    added a function to verify if partner exists before get in /user

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,27 +1,41 @@
 import userModel from '../model/user.model.js'
-var reqUserId;
+import partnerModel from '../model/partner.model.js'
+
+var reqUserId
+var reqPartnerId
 var userDoc
+var partnerDoc
 
 async function getUserDoc(){
-    userDoc = await userModel.findOne({userId: reqUserId}).lean()
+    userDoc = await userModel.findOne({userId: reqUserId, partnerId: reqPartnerId}).lean()
+}
+
+async function getPartnerDoc(){
+    partnerDoc = await partnerModel.findOne({partnerId: reqPartnerId}).lean()
 }
 
 export function getOrCreateUser(req, res) {
     var reqBodyValues = Object.values(req.body);
-    var reqPartnerId = reqBodyValues[0]
+    reqPartnerId = reqBodyValues[0]
     reqUserId = reqBodyValues[1]
 
-    getUserDoc().then(() => {
-        if(userDoc == null){
-            const user = new userModel({
-                userId: reqUserId,
-                partnerId: reqPartnerId
-            })
-            user.save().then(() => {
-                res.status(201).send()
-            })
+    getPartnerDoc().then(() => {
+        if(partnerDoc == null){
+            res.status(404).send()
         } else {
-            res.status(200).send(userDoc)
+            getUserDoc().then(() => {
+                if(userDoc == null){
+                    const user = new userModel({
+                        userId: reqUserId,
+                        partnerId: reqPartnerId
+                    })
+                    user.save().then(() => {
+                        res.status(201).send()
+                    })
+                } else {
+                    res.status(200).send(userDoc)
+                }
+            })
         }
     })
 }
