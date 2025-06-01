dcfe2f4e1dfa9adce06f6f8ceb2a6b2a7ce362a9
matheuscursino <matheuscursino115@gmail.com>
2025-06-01 11:08:07 -0300

    rolled back to old port and not using api subdomain

--- main.js
+++ main.js
@@ -2,14 +2,13 @@ import orderRouter from './route/order.route.js'
 import partnerRouter from './route/partner.route.js'
 
 import express from 'express'
-import subdomain from 'express-subdomain'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import mongoose from 'mongoose'
 import dotenv  from 'dotenv'
 
 const app = express()
-const port = 80
+const port = 3000
 
 dotenv.config()
 
@@ -23,23 +22,19 @@ main().then(()=>{
   console.log(err)
 })
 
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
     extended: true
   }))
 
-app.use(bodyParser.json())
-app.use(cookieParser())
+  app.use(cookieParser())
 
 app.get('/', (req, res) =>  {
     res.send("ok")
 })
 
-const apiRouter = express.Router()
-apiRouter.use('/order', orderRouter)
-apiRouter.use('/partner', partnerRouter)
-app.use(subdomain('api', apiRouter))
-
-
+app.use('/order', orderRouter)
+app.use('/partner', partnerRouter)
 
 app.listen(port, () => {
     console.log(`server started on port: ${port}`)
