bf8d21d949a2f80fc4748cc53788ba0810088c49
matheuscursino <matheuscursino115@gmail.com>
2025-06-29 11:27:06 -0300

    removed updateStatus route

--- controller/order.controller.js
+++ controller/order.controller.js
@@ -133,33 +133,3 @@ export function updatePaymentsOrder(req, res) {
         res.status(500).send()
     })
 }
-
-export function updateStatusOrder(req, res) {
-    var reqBodyValues = Object.values(req.body)
-    reqOrderId = reqBodyValues[0]
-    var reqOrderStatus = reqBodyValues[1]
-    reqAdminPassword = reqBodyValues[2]
-
-    getOrderDoc().then(() => {
-        if(orderDoc == null){
-            res.status(404).send({error: "order doesnt exist"})
-        } else {
-            getAdminDoc().then(() => {
-                if(adminDoc == null){
-                    res.status(403).send()
-                } else{
-                    orderModel.updateOne({orderId: reqOrderId}, {
-                        status: reqOrderStatus,
-                    }).then(() => {
-                        res.status(200).send()
-                    })
-                }
-            })
-            .catch((e) => {
-                res.status(500).send()
-            })
-        }
-    }).catch(() => {
-        res.status(500).send()
-    })
-}
--- route/order.route.js
+++ route/order.route.js
@@ -1,12 +1,11 @@
 import express from 'express'
 var orderRouter = express.Router()
-import { getOrder, createOrder, updatePaymentsOrder, updateStatusOrder } from '../controller/order.controller.js'
+import { getOrder, createOrder, updatePaymentsOrder } from '../controller/order.controller.js'
 
 
 orderRouter.get('/', getOrder)
 orderRouter.post('/', createOrder)
 orderRouter.put('/payments', updatePaymentsOrder)
-orderRouter.put('/status', updateStatusOrder)
 
 
 export default orderRouter;
