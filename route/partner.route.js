912d8e6d51533b755bba1ae90423a5a81a2f9199
matheuscursino <matheuscursino115@gmail.com>
2025-07-05 11:34:04 -0300

    bug fix

--- route/partner.route.js
+++ route/partner.route.js
@@ -1,12 +1,11 @@
 import express from 'express'
 var partnerRouter = express.Router()
-import { getPartner, createPartner, getPartnerByEmail, getPartnerBalance, handlePagarmeWebhook } from '../controller/partner.controller.js'
+import { getPartner, createPartner, getPartnerByEmail, getPartnerBalance } from '../controller/partner.controller.js'
 
 
 partnerRouter.get('/', getPartner)
 partnerRouter.get('/by-email', getPartnerByEmail)
 partnerRouter.post('/', createPartner)
 partnerRouter.get('/balance', getPartnerBalance)
-paymentRouter.post('/webhook', handlePagarmeWebhook)
 
 export default partnerRouter;
