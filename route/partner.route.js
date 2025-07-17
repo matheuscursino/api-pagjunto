27c0fb6d08ee5ad24d1c1d4d526b3fb1f445cf3e
matheuscursino <matheuscursino115@gmail.com>
2025-07-17 14:20:11 -0300

    created create employee route

--- route/partner.route.js
+++ route/partner.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 var partnerRouter = express.Router()
-import { getPartner, createPartner, getPartnerByEmail, getPartnerBalance, updateWebhookUrl } from '../controller/partner.controller.js'
+import { getPartner, createPartner, getPartnerByEmail, getPartnerBalance, updateWebhookUrl, createEmployee } from '../controller/partner.controller.js'
 
 
 partnerRouter.get('/', getPartner)
@@ -8,6 +8,7 @@ partnerRouter.get('/by-email', getPartnerByEmail)
 partnerRouter.post('/', createPartner)
 partnerRouter.get('/balance', getPartnerBalance)
 partnerRouter.put('/webhook', updateWebhookUrl);
+partnerRouter.post('/employee', createEmployee)
 
 
 export default partnerRouter;
